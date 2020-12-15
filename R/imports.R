8d9a1da59e1cebc6e537331c575c14e493c6870a
theordore schrock <theorschrock@gmail.com>
2020-12-15 14:38:09 -0700

    Initial commit

---
+++ R/imports.R
@@ -0,0 +1,14 @@
+#' @import sUtils
+#' @import glue
+#' @import stringr
+#' @import checkmate
+#' @import rlang
+#' @import sDataTable
+#' @import exprTools
+#' @import AOunits
+
+# This script is intended for imports and .onLoad/.onAttach functions only.  Do not add other functions to this file or remove existing.
+
+.onLoad <- function(libname, pkgname) {
+
+}
